02bff8e3782e96e4c29c1a6eb5cc85bca544b4ba
Aoi-U <aoilovepokemon@gmail.com>
2026-01-29 09:01:49 -0700

    optimized texture binding by removing string memory allocations

--- Game/src/Core/Shader.cpp
+++ Game/src/Core/Shader.cpp
@@ -38,9 +38,9 @@ void Shader::setBool(const std::string& name, bool value) const
 	glUniform1i(glGetUniformLocation(ID, name.c_str()), static_cast<int>(value));
 }
 
-void Shader::setInt(const std::string& name, int value) const
+void Shader::setInt(const char* name, int value) const
 {
-	glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+	glUniform1i(glGetUniformLocation(ID, name), value);
 }
 
 void Shader::setFloat(const std::string& name, float value) const
--- Game/src/Core/Shader.h
+++ Game/src/Core/Shader.h
@@ -16,7 +16,7 @@ public:
 	unsigned int getID() const { return ID; }
 
 	void setBool(const std::string& name, bool value) const;
-	void setInt(const std::string& name, int value) const;
+	void setInt(const char* name, int value) const;
 	void setFloat(const std::string& name, float value) const;
 	void setMat4(const std::string& name, const glm::mat4& mat) const;
 	void setVec3(const std::string& name, const float* vec) const;
